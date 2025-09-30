93487433f54d9dbb307a26980f84b0c56a9d9e72
MohammadYehya <mohammad7446@gmail.com>
2025-10-01 03:49:19 +0500

    Update infer.py

--- 092025_RAG-Challenge/infer.py
+++ 092025_RAG-Challenge/infer.py
@@ -1,32 +1,43 @@
 from langchain_graph_retriever.document_graph import create_graph
 from langchain_google_genai import GoogleGenerativeAIEmbeddings 
 from langchain_graph_retriever import GraphRetriever
-from langchain_astradb import AstraDBGraphVectorStore
-from graph_retriever.strategies import Eager
+from langchain_astradb import AstraDBVectorStore
+from graph_retriever.strategies import Mmr
 from visualize import visualize
 import networkx as nx
+import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
-vector_store = AstraDBGraphVectorStore(
+edges = [("number_of_legs", "number_of_legs")]
+
+vector_store = AstraDBVectorStore(
     collection_name="animals2",
     embedding=GoogleGenerativeAIEmbeddings(model="models/text-embedding-004"),
 )
-# simple = GraphRetriever(
-#     store = vector_store,
-#     edges = [("habitat", "habitat"), ("origin", "origin"), ("keywords", "keywords")],
-#     strategy = Eager(k=10, start_k=1, max_depth=2),
-# )
+retriever = GraphRetriever(
+    store = vector_store,
+    edges = edges,
+    strategy = Mmr(k=10, start_k=1, max_depth=2),
+)
 
-# simple_results = simple.invoke("Give me all the insects that have 6 legs.")
+# docs = vector_store.traversal_search("what animals could be found near a capybara?", k=5, max_depth=2, filter=)
 
-# for doc in simple_results:
-#     print(f"{doc.id}: {doc.page_content}\n {doc.metadata}")
+docs = retriever.invoke("What creatures have 8 legs?")
 
-# document_graph = create_graph(
-#     documents=simple_results,
-#     edges = simple.edges,
-# )
+# docs = vector_store.metadata_search(filter={"graph_edges": {"$exists": False}}, n=1000)
 
-document_graph = vector_store.metadata_search(filter={"graph_edges": {"$exists": False}}, n=1000)
-visualize(document_graph)
+# docs = vector_store.metadata_search(n=1000)
+
+for doc in docs:
+    print(f"{doc.id}: {doc.page_content}\n {doc.metadata}")
+    # print(doc)
+
+document_graph = create_graph(
+    documents = docs,
+    edges = edges,
+)
+pos = nx.spring_layout(document_graph)
+nx.draw(document_graph, pos, with_labels=True, node_color="lightblue", arrows=True)
+plt.show()
+# visualize(document_graph)
